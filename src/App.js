bf897bb4ff376322ffcbfe53c31c96bc86a2e893
Jonathan Trowbridge <jonathan.trowbridge@gmail.com>
2018-11-28 20:18:08 -0500

    master - Understanding & Using State
    This chapter explained how class based components have a reserved
    property named state. This property holds the state of the component.
    Functional components do not extend from Component and therefore do not
    have a state property. Functional components should still be used as much as
    possible to avoid having to manage more state across components.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import Person from './Person/Person.js'
 import './App.css';
 
 class App extends Component {
+
+    state = {
+        persons: [
+            {name: 'Jonathan', age: 28}
+            , {name: 'Jeremey', age: 20}
+            , {name: 'Nigel', age: 5}
+        ]
+    };
+
     render() {
         return (
             <div className="App">
-                <Person name="Jonathan" age="28"/>
-                <Person name="Jeremy" age="20"/>
-                <Person name="Nigel" age="5">
-                    Hobbies: Being a blob.
-                </Person>
+                <button>Switch Name</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
             </div>
         );
     }
