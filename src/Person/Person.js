6b43275c3faead254e2ac244363a4e7900e4be64
Jonathan Trowbridge <jonathan.trowbridge@gmail.com>
2018-11-28 19:56:28 -0500

    master - Outputting Dynamic Content
    This chapter focused on the ability of ReactJS components to output
    content dynamically.

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
+const getAge = () => Math.floor(Math.random() * 28);
 
 const person = () => {
     return (
-        <p>I'm alive! :D</p>
+        <p>I'm a person and I'm {getAge()} years old!</p>
     )
 };
 
