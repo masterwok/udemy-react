acc5f3df08f89f06bbe649128f214aa8228e2a18
Jonathan Trowbridge <jonathan.trowbridge@gmail.com>
2018-12-01 09:53:45 -0500

    section-8 - Starting the Burger Component
    This lecture focused on adding the burger to the DOM and applying
    styling.

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
+import styles from './Burger.module.css';
 
 
 export default (props) => {
     return (
-        <>
+        <div className={styles.burger}>
             <BurgerIngredient type={'bread-top'}/>
             <BurgerIngredient type={'salad'}/>
             <BurgerIngredient type={'bacon'}/>
             <BurgerIngredient type={'cheese'}/>
             <BurgerIngredient type={'meat'}/>
             <BurgerIngredient type={'bread-bottom'}/>
-        </>
+        </div>
     );
 }
