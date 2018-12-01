5de8032501a4b3ff31aa93ab13ead8b03aa82079
Jonathan Trowbridge <jonathan.trowbridge@gmail.com>
2018-12-01 10:08:01 -0500

    section-8 - Outputting Burger Ingredients Dynamically
    This lecture focused on passing ingredients to the Burger component.
    This allows us to make the burger component dynamic. This was done by
    passing an object to the Burger component which is then tranformed into
    BurgerIngredient components to be added to the JSX.

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -1,17 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 import styles from './Burger.module.css';
 
 
-export default (props) => {
+/**
+ * Converts an ingredients object to a list of BurgerIngredient components.
+ * @param ingredients The ingredients object.
+ * @returns {any[][]} An array of BurgerIngredient components.
+ */
+const transformIngredients = (ingredients) => Object
+    .keys(ingredients)
+    .map(k => [...Array(ingredients[k])].map((_, i) =>
+        <BurgerIngredient
+            key={`${k}_${i}`}
+            type={k}/>)
+    );
+
+const burger = (props) => {
+
     return (
         <div className={styles.burger}>
             <BurgerIngredient type={'bread-top'}/>
-            <BurgerIngredient type={'salad'}/>
-            <BurgerIngredient type={'bacon'}/>
-            <BurgerIngredient type={'cheese'}/>
-            <BurgerIngredient type={'meat'}/>
+            {transformIngredients(props.ingredients)}
             <BurgerIngredient type={'bread-bottom'}/>
         </div>
     );
-}
+};
+
+burger.propTypes = {
+    ingredients: PropTypes.object.isRequired
+};
+
+export default burger;
--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,11 +4,20 @@ import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends Component {
 
+    state = {
+        ingredients: {
+            salad: 1
+            , bacon: 2
+            , cheese: 2
+            , meat: 2
+        }
+    };
+
     render() {
         return (
             <>
                 <div>Build Controls</div>
-                <Burger/>
+                <Burger ingredients={this.state.ingredients}/>
             </>
         );
     }
