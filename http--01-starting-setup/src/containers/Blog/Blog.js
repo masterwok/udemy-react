7096e4daccbabfc5cd2715a60989cad7fdfc48a8
Jonathan Trowbridge <jonathan.trowbridge@gmail.com>
2018-12-03 20:59:07 -0500

    sectino-9 - Creating a Http Request to GET Data
    This lecture focused on making our first HTTP GET request to a backend
    using axios.

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,24 +1,34 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import axios from 'axios';
 
 class Blog extends Component {
-    render () {
+
+    componentDidMount() {
+        axios.get(
+            'https://jsonplaceholder.typicode.com/posts'
+        ).then(response => {
+            console.log(response);
+        });
+    }
+
+    render() {
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    <Post/>
+                    <Post/>
+                    <Post/>
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost/>
                 </section>
                 <section>
-                    <NewPost />
+                    <NewPost/>
                 </section>
             </div>
         );
